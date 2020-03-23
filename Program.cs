e22a0ad20230b3251fa6c7ce06ccc814e7727ce4
Hlib Davydov <hlib.davydov@ubisoft.com>
2020-03-23 21:03:38 +0200

    Done

--- Program.cs
+++ Program.cs
@@ -13,7 +13,18 @@ namespace HomeworkTemplate
                 // You can get all needed inputs from task.[Property]
                 // Good luck!
 
-                return "My answer";
+                int total = Int32.Parse(task.City.Population);
+                float sickPercentage = float.Parse(task.City.SickPercentage);
+                float mortality = float.Parse(task.Virus.KillProbability);
+
+                float sick = total * sickPercentage;
+                float dead = sick * mortality;
+
+                string virus = task.Virus.Name;
+                string city = task.City.Name;
+
+                return "There are " + Math.Truncate(sick) + " people sick with " + virus + " in the city of " + city + ", " 
+                + Math.Truncate(dead) + " of which died";
             };
 
             Task1.CheckSolver(TaskSolver);
